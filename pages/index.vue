9fdec5ba2da3238331b76c82d4eacbe614288818
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-29 23:29:18 +0300

    First try

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,11 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import UniverseCharacters from '~/components/universe/characters.vue'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    UniverseCharacters
   }
 }
 </script>
